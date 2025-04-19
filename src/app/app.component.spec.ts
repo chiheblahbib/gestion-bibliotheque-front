ffa5c5448c54274bb908fe8c3cce702eae0deaf3
chiheblahbib <159620735+chiheblahbib@users.noreply.github.com>
2025-04-19 17:57:58 +0100

    3rd fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nice-angular-lite'`, () => {
+  it(`should have as title 'gestion-bibliotheque-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nice-angular-lite');
+    expect(app.title).toEqual('gestion-bibliotheque-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nice-angular-lite app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gestion-bibliotheque-front app is running!');
   });
 });
